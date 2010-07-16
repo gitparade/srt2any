2bc4322536ce1db2dd0f4746e6d3897f9270eaa0
ivan <ivan@textzi.net>
2010-07-16 13:50:25 +0200

    We skip alphanumerics-only lines.

--- ingest.pl
+++ ingest.pl
@@ -1,3 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+
 sub ingest {
 	open SRT, $_[0];
+	my $at_the_zone = "not";
+	my %srt_as_hash;
+	my @moment;
+	while (<SRT>) {
+		if ($_ =~ /^\d+$/) { @moment = (); }
+		unless ($_ =~ /^\d+$/) {
+			push @moment, $_;
+		}
+	}
 }
+
+my $subtitles = "soundMarkTompkinsAboutSigiKeil.srt" unless $ARGV[0];
+$subtitles = $ARGV[0] unless not $ARGV[0];
+
+&ingest($subtitles);
